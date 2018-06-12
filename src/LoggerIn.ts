5cd7be1e35dbc00bcc7252c147e7870a3b9dc819
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-12 10:50:46 +0200

    add log in

---
+++ src/LoggerIn.ts
@@ -0,0 +1,58 @@
+import * as data from '/tmp/config.json';
+import * as assert from 'assert';
+import * as datasets from './datasets.json';
+
+const rp = require('request-promise') ;
+
+class LoggerIn {
+
+    url_base: string
+
+    constructor() {
+        this.url_base = "https://scicatapi.esss.dk/api/v2/"
+    }
+
+
+    login_url() {
+        return this.url_base + "Users/login"
+    }
+
+
+    datasets_url() {
+        return this.url_base + "Datasets"
+    }
+
+
+    make_url(temp_url, access_token) {
+        return temp_url + '?access_token=' + access_token;
+    }
+
+    async login() {
+
+        let url = this.login_url();
+        let rawdata = data;
+        console.log(data);
+
+        let options1 = {
+            url: url,
+            method: 'POST',
+            body: rawdata,
+            json: true,
+            rejectUnauthorized: false,
+            requestCert: true
+        };
+        try {
+            const response = await
+                rp(options1);
+            return Promise.resolve(response);
+        }
+        catch (error) {
+            return Promise.reject(error);
+        }
+
+    }
+
+}
+
+
+export {LoggerIn}
