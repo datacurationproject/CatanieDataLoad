cbc1c0f5e03447dec4f3c7a0ea37df0370ddc2f2
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-20 10:44:14 +0200

    🔖

--- src/LoggerIn.ts
+++ src/LoggerIn.ts
@@ -2,6 +2,7 @@
 import * as data from '/tmp/config.json';
 
 const rp = require('request-promise');
+import * as os from 'os';
 
 class LoggerIn {
 
@@ -9,6 +10,25 @@ class LoggerIn {
     login_url: string;
 
     constructor() {
+	        this.url_pick = {
+            "local": "http://localhost:3000",
+            //"macmurphy.local": "http://localhost:3000",
+            "macmurphy.local": "https://scicatapi.esss.dk",
+            //"CI0020036": "http://localhost:3000",
+            "CI0020036": "https://scicatapi.esss.dk",
+            "kubetest01.dm.esss.dk": "https://kubetest02.dm.esss.dk:32223",
+            "scicat01.esss.lu.se": "https://scicat03.esss.lu.se:32223",
+            "dst": "https://scicatapi.esss.dk",
+            "k8s": "http://catamel-dacat-api-server-dev"
+        };
+
+
+        this.machine_name = os.hostname();
+        //this.machine_name = "http://catamel-dacat-api-server-dev"
+        console.log("machine name ", this.machine_name);
+        //this.machine_name = "kubetest01";
+
+        this.url = this.url_pick[this.machine_name];
         this.url_base = "https://scicatapi.esss.dk/api/v2/";
         //this.url_base = "https://kubetest02.dm.esss.dk:32223/api/v2/"
         //this.url_base = "https://scicat03.esss.lu.se:32223/api/v2/"
