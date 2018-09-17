9b0fe7086dbe7f4b956dbee537d6bd6441f8fe8e
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-17 21:50:21 +0200

    update for macm

--- cmd2.sh
+++ cmd2.sh
@@ -3,6 +3,10 @@ if [ "$(hostname)" == "CI0020036" ]; then
 #ssh login "cd test/PyMetadataCreator  && ./generate_metadata.sh"
 	rsync -avz login:test/PyMetadataCreator/test_new_metadata.json src/datasets.json
 fi
+if [ "$(hostname)" == "macmurphy.local" ]; then
+#ssh login "cd test/PyMetadataCreator  && ./generate_metadata.sh"
+	rsync -avz login:test/PyMetadataCreator/test_new_metadata.json src/datasets.json
+fi
 if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
 	rsync -avz ssh0:test_new_metadata.json src/datasets.json
 fi
