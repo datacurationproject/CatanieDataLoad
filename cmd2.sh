fec796fba7eca6e01b7e4db4bccf3fe8d09a520c
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-17 15:02:12 +0200

    Update for kubetest hostname

--- cmd2.sh
+++ cmd2.sh
@@ -3,7 +3,7 @@ if [ "$(hostname)" == "CI0020036" ]; then
 #ssh login "cd test/PyMetadataCreator  && ./generate_metadata.sh"
 	rsync -avz login:test/PyMetadataCreator/test_new_metadata.json src/datasets.json
 fi
-if [ "$(hostname)" == "kubetest01" ]; then
+if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
 	rsync -avz ssh0:test_new_metadata.json src/datasets.json
 fi
 #cp ~/test/PyMetadataCreator/test_new_metadata.json src/datasets.json
