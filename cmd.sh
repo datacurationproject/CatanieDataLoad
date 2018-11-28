37c2aced4068ae84e5a80fd5d85ddd1dd03b4359
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-28 15:05:17 +0100

    rearrange

--- cmd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ "$(hostname)" == "CI0020036" ]; then
-	rsync -avz login:test/PyMetadataCreator/datasets.json src/datasets.json
-fi
-yarn run ts-node src/DeleterOrig.ts
-yarn run ts-node src/Deleter.ts
-yarn run ts-node src/MetadataUploader.ts
