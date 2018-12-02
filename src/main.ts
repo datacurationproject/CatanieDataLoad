9bc880e750d70419fb220c3099fe4d724f0a7822
Gareth Murphy <garethcmurphy@gmail.com>
2018-12-02 07:45:11 +0100

    reuse id from first login

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,12 @@ async function main() {
   const response = await deleteOrig.deleteModel(id);
 
   const deletePub = new DeleterOrig();
-  const id2 = await deletePub.login("default");
   await deletePub.set_model("PublishedData");
-  const response2 = await deletePub.deleteModel(id2);
+  const response2 = await deletePub.deleteModel(id);
 
   const deleteDat = await new DeleterOrig();
-  const id3 = await deleteDat.login("default");
   await deleteDat.set_model("Datasets");
-  const response3 = await deleteDat.deleteModel(id3);
+  const response3 = await deleteDat.deleteModel(id);
 
   const upload = await new MetadataUploader()
   await upload.get_datasets(id);
