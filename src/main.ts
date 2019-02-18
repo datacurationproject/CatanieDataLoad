88f95685ebb4e71c8cce199ec6237885fd232426
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-18 13:40:17 +0100

    add delete to main

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,10 @@ async function main() {
   await deleteDat.set_model("Datasets");
   const response3 = await deleteDat.deleteModel(id);
 
+  const deleteSample = await new DeleterOrig();
+  await deleteSample.set_model("Samples");
+  const response4 = await deleteSample.deleteModel(id);
+
   const upload = await new MetadataUploader()
   await upload.get_datasets(id);
 
